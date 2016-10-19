4f2477ba607e1b7940dc7b0b12154dc3b04a50b1
ZCAH_ThinkCenter_Jimmy@16_10_12 <sd886393@163.com>
2016-10-19 10:26:44 +0800

    leetcode No20 pass asser2

--- leetcode/src/main/java/com/littleji/leetcode/No20_ValidParentheses.java
+++ leetcode/src/main/java/com/littleji/leetcode/No20_ValidParentheses.java
@@ -10,27 +10,25 @@ public class No20_ValidParentheses {
         if (s.length() == 0 || s.length()%2 != 0)
             return false;
         char [] ref = new char[256];
-        ref[(int)'('] = ')';
-        ref[(int)'['] = ']';
-        ref[(int)'{'] = '}';
-/*        ref[(int)'('] = ')';
-        ref[(int)'('] = ')';
-        ref[(int)'('] = ')';
-        ref[(int)'('] = ')';
-        ref[(int)'('] = ')';
-        ref[(int)'('] = ')';
-        ref[(int)'('] = ')';
-        ref[(int)'('] = ')';*/
-        for (int i=0;;){
-            if (s.charAt(i)=='(' && s.charAt(i+1)==')' ||s.charAt(i)=='{'&& s.charAt(i+1)=='}'||s.charAt(i)=='[' && s.charAt(i+1)==']'){
+        Stack<Character> stack = new Stack<Character>();
+        ref[(int)')'] = '(';
+        ref[(int)']'] = '[';
+        ref[(int)'}'] = '{';
+        for (int i=0; i<s.length();i++){
+            if (s.charAt(i)=='('  || s.charAt(i)=='{' || s.charAt(i)=='['  ){
+                stack.push(s.charAt(i));
+            }
+            else if (s.charAt(i)==')' || s.charAt(i)=='}' ||s.charAt(i)==']'){
+                System.out.println(stack.peek());
+                if (stack.peek() == ref[s.charAt(i)])
+                    stack.pop();
             }
             else
                 return false;
-            if (i + 2 >= s.length())
-                break;
-            else
-                i += 2;
         }
-        return true;
+        if (stack.empty())
+            return true;
+        else
+            return false;
     }
 }
--- leetcode/src/test/java/com/littleji/leetcode/TestNo20.java
+++ leetcode/src/test/java/com/littleji/leetcode/TestNo20.java
@@ -10,6 +10,12 @@ public class TestNo20 {
     @Test
     public void assert1(){
         No20_ValidParentheses a = new No20_ValidParentheses();
-        Assert.assertEquals(a.isValid("()"), true);
+        Assert.assertEquals( true,a.isValid("()"));
+    }
+
+    @Test
+    public void assert2(){
+        No20_ValidParentheses a = new No20_ValidParentheses();
+        Assert.assertEquals(true,a.isValid("([])"));
     }
 }
