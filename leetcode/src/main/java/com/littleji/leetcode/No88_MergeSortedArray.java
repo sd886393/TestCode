8b403247dba10f245d6608ffbfda9fd8ce31623a
ZCAH_ThinkCenter_Jimmy@16_10_12 <sd886393@163.com>
2016-10-28 10:10:38 +0800

    add the problem No88

---
+++ leetcode/src/main/java/com/littleji/leetcode/No88_MergeSortedArray.java
@@ -0,0 +1,16 @@
+package com.littleji.leetcode;
+
+/**
+ * Created by Jimmy on 2016/10/27.
+ */
+public class No88_MergeSortedArray {
+    public void merge(int[] nums1, int m, int[] nums2, int n) {
+        if( n == 0)
+            return;
+        else if (m == 0){
+            nums1 = nums2;
+        }
+        int[] nums=new int[5];
+    }
+}
+
