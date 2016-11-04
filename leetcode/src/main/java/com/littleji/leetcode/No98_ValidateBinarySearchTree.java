43d28d767c132964e8d23786a37973721e96a36f
ZCAH_ThinkCenter_Jimmy@16_10_12 <sd886393@163.com>
2016-11-04 17:55:44 +0800

    try something about leetcode problem no98

--- leetcode/src/main/java/com/littleji/leetcode/No98_ValidateBinarySearchTree.java
+++ leetcode/src/main/java/com/littleji/leetcode/No98_ValidateBinarySearchTree.java
@@ -1,2 +1,42 @@
+package com.littleji.leetcode;
+
+import com.littleji.leetcode.utils.BinaryNode;
+
 class No98_ValidateBinarySearchTree{
+    Integer rmin, lmax;
+    public boolean isValidBST(BinaryNode root) {
+        System.out.println(root.data.getClass());
+        if (root.left == null|| root.right == null )
+            return false;
+        Integer data = (Integer)root.getData();
+        boolean result = false;
+        do{
+
+        }while(result );
+
+        return true;
+
+    }
+
+    public boolean doBST(BinaryNode l, BinaryNode r){
+        if (l == null){
+            if (r == null) {
+                lmax = (Integer)l.data;
+                rmin = (Integer)r.data;
+                return true;
+            }
+            else
+                return false;
+        }
+        else if (r == null)
+            return false;
+        
+    }
+
+    public static void main (String [] args){
+        No98_ValidateBinarySearchTree a = new No98_ValidateBinarySearchTree();
+        BinaryNode<Integer>b = new BinaryNode<Integer>(1);
+        a.isValidBST(b);
+    }
 }
+
--- leetcode/src/main/java/com/littleji/leetcode/utils/BinaryNode.java
+++ leetcode/src/main/java/com/littleji/leetcode/utils/BinaryNode.java
@@ -1,10 +1,13 @@
 package com.littleji.leetcode.utils;
 
+import java.lang.reflect.ParameterizedType;
+import java.lang.reflect.Type;
+
 /**
  * Created by Jimmy on 2016/10/25.
  */
 public class BinaryNode<T> {
-    T data;
+    public  T data;
     public  BinaryNode(T data){
         this(data, null, null);
     }
@@ -14,6 +17,19 @@ public class BinaryNode<T> {
         this.left = left;
         this.right = right;
     }
-    BinaryNode<T> left;
-    BinaryNode<T> right;
+    public BinaryNode<T> left;
+    public BinaryNode<T> right;
+    public T getData(){
+        return this.data;
+    }
+    public T getT() throws InstantiationException, IllegalAccessException {
+        Type sType = getClass().getGenericSuperclass();
+        Type[] generics = ((ParameterizedType) sType).getActualTypeArguments();
+        Class<T> mTClass = (Class<T>) (generics[0]);
+        return mTClass.newInstance();
+    }
+    public T analytic2Object(Class<T> tClass, String str)
+                 throws InstantiationException, IllegalAccessException {
+                 return tClass.newInstance();
+             }
 }
