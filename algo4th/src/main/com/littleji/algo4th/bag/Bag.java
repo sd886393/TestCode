699e713d96a300ab0ed2078af2bd4a1a8c9f48fb
ZCAH_ThinkCenter_Jimmy@16_10_12 <sd886393@163.com>
2016-11-18 17:48:36 +0800

    learn the graph in algo4th

---
+++ algo4th/src/main/com/littleji/algo4th/bag/Bag.java
@@ -0,0 +1,8 @@
+package com.littleji.algo4th.bag;
+
+/**
+ * Created by Jimmy on 2016/11/17.
+ */
+public class Bag<Item> {
+
+}
--- algo4th/src/main/com/littleji/algo4th/graph/Graph.java
+++ algo4th/src/main/com/littleji/algo4th/graph/Graph.java
@@ -1,18 +1,24 @@
 package com.littleji.algo4th.graph;
 
 import java.io.BufferedInputStream;
+import java.util.HashSet;
+import java.util.LinkedList;
 
 /**
  * Created by Jimmy on 2016/11/15.
  */
-public class Graph  {
+public class Graph <T>  {
+    private final int V;
+    private int E;
+    private HashSet<LinkedList<T>> adj;
     Graph(int V){
-
+        this.V = V;
     }
-    Graph(BufferedInputStream in){
-
+    Graph(BufferedInputStream in, int V){
+        this.V = V;
     }
-    int V(){
-        return
+    public int V(){
+        return this.V;
     }
+    public int E() { return this.E; }
 }
