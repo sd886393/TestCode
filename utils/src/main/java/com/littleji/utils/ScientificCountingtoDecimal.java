ba05f5fe929b456f3bd8fb0ce27b29c47b11ba2b
ZCAH_ThinkCenter_Jimmy@16_10_12 <sd886393@163.com>
2016-11-25 11:14:02 +0800

    for double to decimal number

---
+++ utils/src/main/java/com/littleji/utils/ScientificCountingtoDecimal.java
@@ -0,0 +1,19 @@
+package com.littleji.utils;
+
+/**
+ * Created by Jimmy on 2016/11/24.
+ */
+public class ScientificCountingtoDecimal {
+    private  double  value=0;
+    ScientificCountingtoDecimal(String org){
+        value = Double.parseDouble(org);
+    }
+
+    @Override
+    public String toString() {
+        return super.toString();
+    }
+    public void main (){
+        String a = ""
+    }
+}
