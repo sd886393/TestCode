b9e4058c6e134a4142a48e1f32b9458ca3024736
sd886393 <sd886393@163.com>
2018-02-04 19:46:04 +0800

    go学习的测试程序

---
+++ go/hello.go
@@ -0,0 +1,6 @@
+package main
+
+// func main() {
+// 	who := "World!"
+// 	fmt.Println("Hello", who)
+// }
---
+++ go/stack.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"fmt"
+)
+
+const (
+	a = 2
+	b = iota
+	c = iota - 1
+	d = iota
+)
+
+const (
+	e = 2
+	f = iota
+)
+
+func main() {
+	x := 10
+	var y int32 = 20
+	z := x + int(y)
+	fmt.Println(x, y, z)
+
+}
