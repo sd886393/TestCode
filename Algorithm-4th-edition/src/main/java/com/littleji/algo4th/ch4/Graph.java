5cf7357a3d82be8812f6dee27d74db2278f70d7c
littleji <littlejicom@gmail.com>
2018-12-04 22:57:30 +0600

    增加Princeton的算法库

--- Algorithm-4th-edition/src/main/java/com/littleji/algo4th/ch4/Graph.java
+++ Algorithm-4th-edition/src/main/java/com/littleji/algo4th/ch4/Graph.java
@@ -1,7 +1,32 @@
 package com.littleji.algo4th.ch4;
 
-public class Graph {
-    Graph(int V);
 
+import edu.princeton.cs.introcs.StdRandom;
+
+public class Graph {
+
+    Graph(int V) {
+
+    }
+
+    public static void change1(int [] p){
+        System.out.println(p);
+        p = new int[]{2};
+        System.out.println(p);
+    }
+
+    public static void change2(int p){
+        System.out.println(p);
+        p = 3;
+        System.out.println(p);
+    }
+    public static void main(String [] args){
+        int [] a = {1};
+        int b = 2;
+        System.out.println(b);
+        change1(a);
+        change2(b);
+        StdRandom.uniform();
+    }
 
 }
